162a4c0ca28b8c39682cdb0795f51bedc6ceeafb
Karl Glaser <kg@papercloud.com.au>
2013-11-15 23:26:07 +1100

    grunt-init commonjs

---
+++ dist/audio.js
@@ -0,0 +1,12 @@
+/*! audio - v0.1.0 - 2013-11-15
+* https://github.com/kg/audio
+* Copyright (c) 2013 Karl Glaser; Licensed MIT */
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
