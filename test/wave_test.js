1683a98a85554f8fa055f03e117597932e3794ec
Karl Glaser <kg@papercloud.com.au>
2013-11-16 01:50:44 +1100

    wave plays

--- test/wave_test.js
+++ test/wave_test.js
@@ -4,16 +4,30 @@ me = me.wave;
 
 exports['osc'] = {
   'will connect': function(test) {
-    test.expect(1);
     var stub = {};
     stub.createOscillator = function() {
       var obj = {};
+      obj.noteOn = function() {
+        test.ok(true);
+      };
       obj.connect = function() {
         test.ok(true);
       };
       return obj;
     };
     me.osc(stub);
+    test.done();
+  }
+};
+
+exports['kill'] = {
+  'will disconnect': function(test) {
+    test.expect(1);
+    var stub = {};
+    stub.disconnect = function() {
+      test.ok(true);
+    };
+    me.kill(stub);
     test.done();
   }
 };
