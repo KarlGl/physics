c3a33eaf3e8d05fa28c6c22bb18da99437812751
Karl Glaser <kg@papercloud.com.au>
2013-11-16 00:51:24 +1100

    image parser getfreqs

--- test/image_parser_test.js
+++ test/image_parser_test.js
@@ -2,17 +2,42 @@
 var ipar = require('../lib/image_parser.js');
 ipar = ipar.imageParser;
 
+exports['getFreqs'] = {
+  'empty': function(test) {
+    test.deepEqual(ipar.getFreqs(0, 0, function() {
+      return {
+        data: [255, 0, 0, 0]
+      };
+    }), []);
+    test.done();
+  },
+  'calls get for x twice': function(test) {
+    var _getFreqsForX = ipar.getFreqsForX;
+    ipar.getFreqsForX = function() {
+      return 'foo';
+    };
+    test.deepEqual(ipar.getFreqs(2, 0, function() {
+      return {
+        data: [255, 0, 0, 0]
+      };
+    }), ['foo', 'foo']);
+    ipar.getFreqsForX = _getFreqsForX;
+    test.done();
+  },
+};
+
 exports['getFreqsForX'] = {
-  'no args': function(test) {
-    test.expect(2);
-    // tests here
+  '1': function(test) {
     test.deepEqual(ipar.getFreqsForX(0, 1, function() {
       return {
         data: [255, 0, 0, 0]
       };
     }), [
       [255, 0, 0, 0]
-    ], '1.');
+    ]);
+    test.done();
+  },
+  'empty': function(test) {
     test.deepEqual(ipar.getFreqsForX(0, 0, function() {
       return {
         data: [255, 0, 0, 0]
